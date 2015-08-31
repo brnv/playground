38109c49d4db062187af36401aada779ffdfbff0
Alexey Baranov <baranov.alxy@gmail.com>
2015-09-01 01:24:18 +0600

    another content parser

--- python/crawler/bodymaster.py
+++ python/crawler/bodymaster.py
@@ -1,13 +1,13 @@
 import scrapy
 
-class Spider(scrapy.Spider):
+class BodyMasterCrawler(scrapy.Spider):
 
     name = 'anyrandomname'
 
     base_url = 'http://bodymaster.sportbox.ru/exercises/?PAGEN_1='
 
     def start_requests(self):
-        for page_num in range(78):
+        for page_num in range(79):
             exercises_page_url = self.base_url + str(page_num)
             yield scrapy.Request(
                     exercises_page_url, callback=self.parse_exercises_list_page)
---
+++ python/crawler/pumpiron.py
@@ -0,0 +1,41 @@
+import scrapy
+
+class PumpIronCrawler(scrapy.Spider):
+
+    name = 'anyrandomname'
+
+    start_urls = [
+        'http://pumpiron.ru/exercize/muscle/biceps',
+        'http://pumpiron.ru/exercize/muscle/grud',
+        'http://pumpiron.ru/exercize/muscle/ikry',
+        'http://pumpiron.ru/exercize/muscle/nogi',
+        'http://pumpiron.ru/exercize/muscle/plechi',
+        'http://pumpiron.ru/exercize/muscle/predplechya',
+        'http://pumpiron.ru/exercize/muscle/press',
+        'http://pumpiron.ru/exercize/muscle/spina',
+        'http://pumpiron.ru/exercize/muscle/trapeciya',
+        'http://pumpiron.ru/exercize/muscle/triceps',
+        'http://pumpiron.ru/exercize/muscle/sheya',
+        'http://pumpiron.ru/exercize/muscle/yagodicy',
+    ]
+
+    def parse(self, response):
+        for page_num in range(3):
+            exercises_page_url = response.urljoin('?page=' + str(page_num))
+            yield scrapy.Request(
+                    exercises_page_url, callback=self.parse_exercises_list_page)
+
+    def parse_exercises_list_page(self, response):
+        for href in response.css('h2.exercize_group_title a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_exercise)
+
+    def parse_exercise(self, response):
+        yield {
+            'title': response.css('h1.content_title::text').extract(),
+            'description': response.css('div.field-type-text-with-summary div.field-item p::text').extract(),
+            'images': response.css('div.field-type-image img::attr(src)').extract(),
+            'muscles_base': response.css('div.view-display-id-muscle_base a::text').extract(),
+            'muscles_sub': response.css('div.view-display-id-muscle_sub a::text').extract(),
+            'link': response.url,
+        }
